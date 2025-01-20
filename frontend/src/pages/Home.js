2783e7deeaef6b8cdd1fe86f3d111f8dbb0703c7
Manoranjan Kumar <108667030+Manoranjan-Kumar555@users.noreply.github.com>
2025-01-20 11:34:43 +0530

    Update Home.js
    dev api link updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8080/products";
+      const url = "https://registration-deploy-mern-apps-api6.vercel.app/products";
       const headers = {
         headers: {
           Authorization: localStorage.getItem("token"),
